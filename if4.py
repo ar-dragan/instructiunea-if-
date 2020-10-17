63e464c6ac35983b6a87447cf5bed2bb281aff3a
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-17 23:22:28 +0300

    Add files via upload

---
+++ if4.py
@@ -0,0 +1,9 @@
+a=int(input("Introduceti varsta primei persoane:"))
+b=int(input("Introduceti varsta la a doua persoana:"))
+c=int(input("Introduceti varsta la a treia persoana:"))
+if a>18 and a<60:
+    print(a)
+if b>18 and b<60:
+    print(b)
+if c>18 and c<60:
+    print(c)
