feb576dc072ba7c8ad36aa9204afe5fb8756378d
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-17 23:30:08 +0300

    Add files via upload

---
+++ if6.py
@@ -0,0 +1,10 @@
+n1=int(input("introduceti prima nota:"))
+n2=int(input("introduceti a doua nota:"))
+n3=int(input("introduceti a treia nota:"))
+if n3>8:
+   print(n1,n2,n3)
+if n3<8:
+  if n1>n2:
+   print(n1)
+else:
+   print(2)
