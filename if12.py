034876b1dad326eb6e0dcbe17fefe44a6fc737af
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-17 23:59:28 +0300

    Add files via upload

---
+++ if12.py
@@ -0,0 +1,11 @@
+a=int(input("Numarul de petale"))
+if a%5==1:
+    print("ma iubeste un pic")
+elif a%5==2:
+    print("ma iubeste mult")
+elif a%5==3:
+    print("ma iubeste cu pasiune")
+elif a%5==4:
+    print("ma iubeste la nebunie")
+else:
+    print("ma iubeste de loc")
