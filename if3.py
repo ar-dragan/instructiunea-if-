bf9676572eeb08002ec9325fe7a61a1143be7528
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-17 23:17:08 +0300

    Add files via upload

---
+++ if3.py
@@ -0,0 +1,5 @@
+l=input("Introduceti o litera:")
+if (l=='A' or l=='a' or l=='E' or l =='e' or l=='I'or l=='i' or l=='O' or l=='o' or l=='U' or l=='u' or l=='Ă' or l=='ă' or l=='Î' or l=='î' or l=='Â' or l=='â'):
+    print(l,"este o vocala")
+else:
+    print(l,"este o consoana")
